54151a7c1de6c1cc0b269b73e1f471af4143c39d
ADMIN <ADMIN@DESKTOP-44SJU5V>
2021-11-12 17:47:51 +0530

    Yes

--- SDET_24githubPractice/src/test/java/GithubPractice/Github1.java
+++ SDET_24githubPractice/src/test/java/GithubPractice/Github1.java
@@ -8,6 +8,12 @@ public class Github1
  @Test
  public void Githubmain()
  {
+	 String browser=System.getProperty("browser");
+     String url=System.getProperty("url");
+     System.out.println(browser);
+     System.out.println(url);
+     
+     
 	 System.out.println("successful");
 	 System.out.println("Chaitanya");
 	 System.out.println("vidya");
@@ -15,7 +21,7 @@ public class Github1
 	 System.out.println("ab");
 	 System.out.println("abc5");
 
-	 System.out.println("abc5");
+	 
 
 	 
  }
