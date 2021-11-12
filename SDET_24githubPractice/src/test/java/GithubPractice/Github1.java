a09b5b9c814b3df17aa9fd1d63f395e14f519b0d
ADMIN <ADMIN@DESKTOP-44SJU5V>
2021-11-12 17:05:02 +0530

    try

--- SDET_24githubPractice/src/test/java/GithubPractice/Github1.java
+++ SDET_24githubPractice/src/test/java/GithubPractice/Github1.java
@@ -11,7 +11,7 @@ public class Github1
 	 System.out.println("successful");
 	 System.out.println("Chaitanya");
 	 System.out.println("vidya");
-	 System.out.println("Shri");
+
 	 System.out.println("abcd");
 	 System.out.println("ab");
 	 System.out.println("abc5");
